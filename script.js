cfadad6acb427143b752dff0b77603eaff5d3352
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2025-02-15 14:03:32 +0200

    replace xhr with fetch

--- script.js
+++ script.js
@@ -1,55 +1,59 @@
-renderPage();
-function renderPage() {
-  renderUsersSection();
-}
-function renderUsersSection() {
-  const req = new XMLHttpRequest();
-  req.open("GET", "https://jsonplaceholder.typicode.com/users");
-  req.responseType = "json";
-  req.onload = function () {
-    let users = req.response;
-    users.forEach((user) => {
-      document.querySelector(".users").innerHTML += `
-    <div class="user" data-user-id=${user.id}>
-    <h2 class="user-name">${user.name}</h2>
-    <p class="user-email">${user.email}</p>
-  </div>
-  `;
-    });
-    document.querySelectorAll(".user").forEach((user) => {
-      user.addEventListener("click", function () {
+fetchAllUsers().then((firstUserId) => {
+  fetchPostsOf(firstUserId);
+});
+function fetchAllUsers() {
+  return fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => {
+      if (users.length) {
+        const firstUserId = users[0].id;
+        document.querySelector(".users").innerHTML = users
+          .map((user) => {
+            return `<div class="user" data-user-id=${user.id}>
+            <h2 class="user-name">${user.name}</h2>
+            <p class="user-email">${user.email}</p>
+        </div>
+      `;
+          })
+          .join("");
         document.querySelectorAll(".user").forEach((user) => {
-          user.classList.remove("active");
+          user.addEventListener("click", function () {
+            document.querySelector(".active").classList.remove("active");
+            this.classList.add("active");
+            const userId = this.dataset.userId;
+            fetchPostsOf(userId);
+          });
         });
-        this.classList.toggle("active");
-        const userId = this.dataset.userId;
-        console.log(userId);
-        renderPostsSection(userId);
-      });
+        document.querySelector(".user").classList.add("active");
+        return firstUserId;
+      } else {
+        document.querySelector(".users").innerHTML = `
+          <h1 style="margin-top:50px;color:whiteSmoke"><i>Oops!</i><br/> Can't find any users</h1> 
+        `;
+        return Promise.reject();
+      }
     });
-    document.querySelector(".user").classList.add("active");
-    renderPostsSection(1);
-  };
-  req.send();
 }
-function renderPostsSection(userId) {
-  const req = new XMLHttpRequest();
-  req.open(
-    "GET",
-    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
-  );
-  req.responseType = "json";
-  req.onload = function () {
-    const posts = req.response;
-    document.querySelector(".posts").innerHTML = "";
-    posts.forEach((post) => {
-      document.querySelector(".posts").innerHTML += `
-      <div class="post">
-          <h2 class="post-title">${post.title}</h2>
-          <p class="post-body">${post.body}</p>
-      </div>
- `;
+function fetchPostsOf(userId) {
+  fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    .then((response) => response.json())
+    .then((posts) => {
+      if (posts.length) {
+        document.querySelector(".posts").innerHTML = posts
+          .map(
+            (post) =>
+              `
+              <div class="post">
+                  <h2 class="post-title">${post.title}</h2>
+                  <p class="post-body">${post.body}</p>
+              </div>
+            `
+          )
+          .join("");
+      } else {
+        document.querySelector(".posts").innerHTML = `
+          <h1 style="margin-top:50px;font-style:italic">This user hasn't posted yet ..</h1> 
+        `;
+      }
     });
-  };
-  req.send();
 }
